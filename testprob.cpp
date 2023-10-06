782e50f5e41edf4ebb4c12cbe192836bbe9c6e5b
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2023-10-06 03:15:41 -0700

    Added c++ program for word count
    Tester function before we implement with quorum

---
+++ testprob.cpp
@@ -0,0 +1,53 @@
+/// @brief This program counts the number of times each word occurs in a file.
+/// @author Willie Alcaraz
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    // file variables
+    ifstream inFile;
+    string filename = "input0.txt";
+
+    // loop until file is opened
+    while (!inFile.is_open()) {
+
+        cout << filename << endl;
+        inFile.open(filename.c_str());
+        // prompt if input file failure
+        if (!inFile.is_open()) {
+            cout << "Error opening file." << endl;
+            cout << "Enter filename: ";
+            cin >> filename;
+        }
+        // repeat if necessary
+    }
+
+    // input contents of file to map
+    map<string, int> wordCount;
+    string word;
+    while (inFile >> word) {
+        wordCount[word]++;
+    }
+
+    // output words, along with number of occurences per word
+    cout << setw(15) << left << "Word" << setw(15) << "Count" << endl;
+
+    // use iterator for map, iterate through each existing word
+    map<string, int>::iterator iter = wordCount.begin();
+    while (iter != wordCount.end()) {
+        cout << setw(15) << left << iter->first << setw(15) << iter->second << endl;
+        iter++;
+    }
+
+    // close file
+    inFile.close();
+
+    return 0;
+}
